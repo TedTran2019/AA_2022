aa5c8891be6636e2aef85d4d6ed7872cf2d43ea8
Ted Tran <tedtran2019@gmail.com>
2022-04-09 22:47:29 -0700

    Completed in_check and preparing for final phase

--- w2d1/chess/board.rb
+++ w2d1/chess/board.rb
@@ -9,7 +9,7 @@ class Board
     @display = Display.new(self)
   end
 
-  # Once players are factored in, valid_move only takes if str_pos is same color
+  # Once players are factored in, valid_move only takes if start_pos is same color
   # Also factor in check into available moves later on
   def move_piece(start_pos, end_pos)
     return false unless valid_move?(start_pos, end_pos)
@@ -23,6 +23,37 @@ class Board
     return false unless self[start_pos].space_taken?
 
     self[start_pos].moves.include?(end_pos)
+  end
+
+  # A hash would be much better than an array here
+  def in_check?(color)
+    king_pos = find_piece(color, King)
+    @grid.each do |row|
+      @row.each do |piece|
+        next if piece.color.nil?
+        return true if piece.color != color && piece.moves.include?(king_pos)
+      end
+    end
+    false
+  end
+
+  # This will probably need to be fixed
+  def checkmate?(color)
+    in_check?(color) && valid_moves(color).empty?
+  end
+
+  def find_piece(color, piece)
+    (0...8).each do |y|
+      (0...8).each do |x|
+        pos = [y, x]
+        return pos if self[pos].is_a?(piece) && self[pos].color == color
+      end
+    end
+    nil
+  end
+
+  def checkmate?(color)
+
   end
 
   def space_taken?(pos)
@@ -88,6 +119,3 @@ class Board
     end
   end
 end
-
-b = Board.new
-b.display.test
