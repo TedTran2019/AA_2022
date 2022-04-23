ee1e2eaec849b4fe0389a0c3060d7e32dfedc125
Ted Tran <tedtran2019@gmail.com>
2022-04-23 10:12:28 -0700

    Figured out why Flash.now doesn't work. Have to add status message or it won't render

--- w4d4/music_app/app/controllers/tracks_controller.rb
+++ w4d4/music_app/app/controllers/tracks_controller.rb
@@ -12,7 +12,7 @@ class TracksController < ApplicationController
       redirect_to track_url(@track)
     else
       flash.now[:errors] = @track.errors.full_messages
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
   
@@ -32,7 +32,7 @@ class TracksController < ApplicationController
       redirect_to track_url(@track)
     else
       flash.now[:errors] = @track.errors.full_messages
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
