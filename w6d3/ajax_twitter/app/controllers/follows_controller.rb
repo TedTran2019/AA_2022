c33ade08291dd99e995d534d01f9e72a44caa6bb
Ted Tran <tedtran2019@gmail.com>
2022-06-25 19:03:19 -0700

    Completed final part. Lots of reviews

--- w6d3/ajax_twitter/app/controllers/follows_controller.rb
+++ w6d3/ajax_twitter/app/controllers/follows_controller.rb
@@ -9,7 +9,8 @@ class FollowsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to request.referrer }
-      format.json { render json: @follow }
+      # format.json { render json: @follow }
+      format.json { render :show }
     end
   end
 
@@ -21,7 +22,8 @@ class FollowsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to request.referrer }
-      format.json { render json: @follow }
+      # format.json { render json: @follow }
+      format.json { render :show }
     end
   end
 end
--- w6d3/ajax_twitter/app/controllers/tweets_controller.rb
+++ w6d3/ajax_twitter/app/controllers/tweets_controller.rb
@@ -1,6 +1,14 @@
 class TweetsController < ApplicationController
   before_action :require_logged_in!
 
+  def index
+    @tweets = Tweet.all.includes(:user, :mentioned_users)
+    respond_to do |format|
+      format.html { redirect_to request.referrer }
+      format.json { render :index }
+    end
+  end
+
   def create
     # simulate latency
     sleep(1)
