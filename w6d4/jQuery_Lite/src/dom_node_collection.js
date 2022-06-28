1dee0061bb3dc9210587611b803ea0e2abe8b511
Ted Tran <tedtran2019@gmail.com>
2022-06-27 19:17:13 -0700

    Committed jQuery lite

--- w6d4/jQuery_Lite/src/dom_node_collection.js
+++ w6d4/jQuery_Lite/src/dom_node_collection.js
@@ -7,7 +7,7 @@ class DomNodeCollection {
   // Empty string is false in Javascript
   html(arg) {
     if (arg === undefined) {
-      if (this.htmlElements.length === 1) {
+      if (this.htmlElements.length > 0) {
         return this.htmlElements[0].innerHTML;
       }
     } else {
@@ -44,40 +44,128 @@ class DomNodeCollection {
     }
   }
 
-  attr() {
-
+  attr(name, value) {
+    if (value === undefined) {
+      if (this.htmlElements.length > 0) {
+        return this.htmlElements[0].getAttribute(name);
+      }
+    } else {
+      this.htmlElements.forEach(el => {
+        el.setAttribute(name, value);
+      });
+    }
   }
 
-  addClass() {
-
+  addClass(className) {
+    this.htmlElements.forEach(el => {
+      el.classList.add(className);
+    })
   }
 
-  removeClass() {
-
+  removeClass(className) {
+    if (className === undefined) {
+      this.htmlElements.forEach(el => {
+        el.className = "";
+      })
+    } else {
+      this.htmlElements.forEach(el => {
+        el.classList.remove(className);
+      })
+    }
   }
 
   children() {
-
+    const children = [];
+    this.htmlElements.forEach(el => {
+      const eachChildren = el.children
+      for (let i = 0; i < eachChildren.length; i++) {
+        children.push(eachChildren[i]);
+      }
+    })
+    return new DomNodeCollection(children);
   }
 
   parent() {
-
+    const parents = []
+    this.htmlElements.forEach(el => {
+      parents.push(el.parentElement);
+    })
+    return new DomNodeCollection(parents);
   }
 
-  find() {
-
+  find(selector) {
+    const found = [];
+    this.htmlElements.forEach(el => {
+      const foundChildren = el.querySelectorAll(selector);
+      for (let i = 0; i < foundChildren.length; i++) {
+        found.push(foundChildren[i]);
+      }
+    })
+    return new DomNodeCollection(found);
   }
 
-  remove() {
-
+  remove(selector) {
+    this.htmlElements.forEach(el => {
+      if (selector === undefined) {
+        el.remove();
+      } else {
+        const found = el.querySelectorAll(selector);
+        for (let i = 0; i < found.length; i++) {
+          found[i].remove();
+        }
+      }
+    })
   }
 
-  on() {
-
+  on(event, handler) {
+    this.htmlElements.forEach(el => {
+      el.addEventListener(event, handler);
+      if (el.listen === undefined) {
+        el.listen = {};
+      }
+      if (el.listen[event] === undefined) {
+        el.listen[event] = [handler];
+      } else {
+        el.listen[event].push(handler);
+      }
+    })
   }
 
-  off() {
-
+  // Handler not provided: remove all of type === event
+  // Event and handler not provided: remove all events
+  off(event, handler) {
+    if (event === undefined && handler === undefined) {
+      this.htmlElements.forEach(el => {
+        const listen = el.listen;
+        for (const elEvent in listen) {
+          listen[elEvent].forEach(elHandler => {
+            el.removeEventListener(elEvent, elHandler);
+          })
+        }
+        el.listen = undefined;
+      })
+    } else if (handler === undefined) {
+      this.htmlElements.forEach(el => {
+        const listen = el.listen;
+        listen[event].forEach(elHandler => {
+          el.removeEventListener(event, elHandler);
+        })
+        el.listen[event] = undefined;
+      })
+    } else {
+      this.htmlElements.forEach(el => {
+        const listen = el.listen;
+        const newArray = [];
+        listen[event].forEach(elHandler => {
+          if (elHandler.toString() !== handler.toString()) {
+            newArray.push(elHandler);
+          } else {
+            el.removeEventListener(event, elHandler);
+          }
+        })
+        el.listen[event] = newArray;
+      })
+    }
   }
 }
 
--- w6d4/jQuery_Lite/src/index.js
+++ w6d4/jQuery_Lite/src/index.js
@@ -1,5 +1,8 @@
 const DomNodeCollection = require('./dom_node_collection');
 
+let isReady = false;
+let readyCallbacks = [];
+
 window.$1 = function(selector) {
   if (typeof selector === 'string') {
     const nodeList = document.querySelectorAll(selector);
@@ -8,9 +11,59 @@ window.$1 = function(selector) {
   } else if (selector instanceof HTMLElement) {
     return new DomNodeCollection([selector]);
   } else if (typeof selector === 'function') {
-    window.addEventListener('DOMContentLoaded', selector);
+    // window.addEventListener('DOMContentLoaded', selector); Adds too many event listeners
+    if (isReady) {
+      selector();
+    } else {
+      readyCallbacks.push(selector);
+    }
   }
 }
+
+window.$1.extend = function(objA, ...objects) {
+  objects.forEach(obj => {
+    for (key in obj) {
+      objA[key] = obj[key];
+    }
+  })
+  return objA;
+}
+
+window.$1.ajax = function(options) {
+  const defaults = {
+    // async: true,
+    contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+    data: {},
+    // dataType: 'json',
+    // global: true,
+    // headers: {},
+    // ifModified: false,
+    method: 'GET',
+    // processData: true,
+    // statusCode: {},
+    // type: 'GET',
+    url: window.location.href,
+    success: () => {},
+    error: () => {}
+  }
+  options = $1.extend(defaults, options);
+  const xhr = new XMLHttpRequest();
+  xhr.open(options.method, options.url);
+  xhr.onload = function() {
+    let response = JSON.parse(xhr.response);
+    if (xhr.status === 200) {
+      options.success(response);
+    } else {
+      options.error(response);
+    }
+  }
+  xhr.send(options.data);
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  readyCallbacks.forEach(callback => callback());
+  isReady = true;
+})
 
 // html 
 // window.$1('.test1').html()
@@ -27,14 +80,75 @@ window.$1 = function(selector) {
 // $('.test1').empty()
 
 // append
-a = 'hello';
-b = document.querySelector('.test2')
-c = window.$1('.test2')
-d = $('.test2')
-window.$1('.test1').append(a)
-window.$1('.test1').append(b)
-window.$1('.test1').append(c)
-// jQuery Append moves the oject to the specified location instead of making a copy
-$('.test1').append(a)
-$('.test1').append(b)
-$('.test1').append(d)
+// a = 'hello';
+// b = document.querySelector('.test2')
+// c = window.$1('.test2')
+// d = $('.test2')
+// window.$1('.test1').append(a)
+// window.$1('.test1').append(b)
+// window.$1('.test1').append(c)
+// // jQuery Append moves the oject to the specified location instead of making a copy
+// $('.test1').append(a)
+// $('.test1').append(b)
+// $('.test1').append(d)
+
+// attr
+// name = 'name';
+// value = 'Steve';
+// window.$1('.test1').attr(name)
+// window.$1('.test1').attr(name, value)
+// window.$1('.test1').attr(name)
+
+// addClass
+// window.$1('.test1').addClass('testing')
+
+// removeClass
+// window.$1('.test1').addClass('testing')
+// window.$1('.test1').addClass('blargh')
+// window.$1('.test1').removeClass('potato')
+// window.$1('.test1').removeClass('testing')
+// window.$1('.test1').addClass('testing-again')
+// window.$1('.test1').removeClass()
+
+// children
+// window.$1('.test6').children()
+
+// parent
+// window.$1('.test1').parent()
+
+// find
+// window.$1('body').find('.test1')
+
+// remove
+// window.$1('.test6').remove()
+// window.$1('.test6').remove('.test7')
+
+// on
+// window.$1('.test1').on('click', () => { console.log('Hello') })
+// window.$1('.test1').htmlElements[0].listen.click[0]()
+
+// off
+// window.$1('.test1').on('click', () => { console.log('Hello') })
+// window.$1('.test1').on('click', () => { console.log('Goodbye') })
+// window.$1('.test1').on('mouseup', () => { console.log('Blargh') })
+// window.$1('.test1').htmlElements[0].listen
+// window.$1('.test1').off()
+// window.$1('.test1').htmlElements[0].listen
+// Re-setup Hello, Goodbye, Blargh
+// window.$1('.test1').off('click')
+// window.$1('.test1').htmlElements[0].listen
+// window.$1('.test1').off('mouseup', () => { console.log('Blargh') })
+// window.$1('.test1').htmlElements[0].listen
+
+// Testing $(function)
+// window.$1(() => alert("the document is ready"));
+
+// Testing extend
+// const objA = { a: "a", b: "a", c: "a" };
+// const objB = { b: "b", c: "b" };
+// const objC = { c: "c" };
+// $l.extend(objA, objB, objC); 
+// console.log(objA);
+
+// Testing ajax
+// window.$1.ajax({ success: () => { console.log('success') }, failure: () => { console.log('failure') } })
